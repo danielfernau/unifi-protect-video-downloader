31aa3ee7ebb55c736d22cf2febc194039fffbc63
Daniel Fernau <github@danielfernau.de>
2021-05-02 02:04:11 +0200

    update client module to support UniFi OS

--- protect_archiver/client/__init__.py
+++ protect_archiver/client/__init__.py
@@ -1,32 +1,34 @@
 from os import path
 
-from protect_archiver.client.uck import CloudKeyClient
-from protect_archiver.client.udm import DreamMachineClient
+from protect_archiver.client.legacy import LegacyClient
+from protect_archiver.client.unifi_os import UniFiOSClient
 from protect_archiver.config import Config
 from protect_archiver.downloader.get_camera_list import get_camera_list
+from protect_archiver.downloader.get_motion_event_list import get_motion_event_list
 
 
-# TODO(danielfernau): In Python3 all classes are new-style classes, parameter 'object' is no longer necessary, remove it
-class ProtectClient(object):
+class ProtectClient:
     def __init__(
         self,
-        address: str,
-        hardware_type: str,
-        username: str = Config.General.USERNAME,
-        password: str = Config.General.PASSWORD,
-        verify_ssl: bool = Config.General.VERIFY_SSL,
-        use_unsafe_cookie_jar: bool = False,
-        ignore_failed_downloads: bool = False,
-        download_wait: int = 0,
-        use_subfolders: bool = False,
-        skip_existing_files: bool = False,
-        destination_path: str = "./",
-        touch_files: bool = False,
+        address: str = Config.ADDRESS,
+        protocol: str = Config.PROTOCOL,
+        username: str = Config.USERNAME,
+        password: str = Config.PASSWORD,
+        verify_ssl: bool = Config.VERIFY_SSL,
+        not_unifi_os: bool = False,
+        # use_unsafe_cookie_jar: bool = Config.USE_UNSAFE_COOKIE_JAR,
+        ignore_failed_downloads: bool = Config.IGNORE_FAILED_DOWNLOADS,
+        download_wait: int = Config.DOWNLOAD_WAIT,
+        use_subfolders: bool = Config.USE_SUBFOLDERS,
+        skip_existing_files: bool = Config.SKIP_EXISTING_FILES,
+        destination_path: str = Config.DESTINATION_PATH,
+        touch_files: bool = Config.TOUCH_FILES,
         # aka read_timeout - time to wait until a socket read response happens
-        download_timeout: float = 60.0,
+        download_timeout: float = Config.DOWNLOAD_TIMEOUT,
     ):
+        self.protocol = protocol
         self.address = address
-        self.hardware_type = hardware_type
+        self.not_unifi_os = not_unifi_os
         self.username = username
         self.password = password
         self.verify_ssl = verify_ssl
@@ -43,27 +45,44 @@ class ProtectClient(object):
         self.files_downloaded = 0
         self.bytes_downloaded = 0
         self.files_skipped = 0
+        self.files_failed = 0
         self.max_retries = 3
 
         self._access_key = None
         self._api_token = None
 
-        # TODO
-        if self.hardware_type == "uck":
-            self.port = Config.CloudKey.PORT
-            self.session = CloudKeyClient(
-                self.address, self.port, self.username, self.password, self.verify_ssl
+        if not_unifi_os:
+            self.port = 7443
+            self.base_path = "/api"
+            self.session = LegacyClient(
+                self.protocol,
+                self.address,
+                self.port,
+                self.username,
+                self.password,
+                self.verify_ssl,
             )
-        elif self.hardware_type == "udm":
-            self.port = Config.DreamMachine.PORT
-            self.session = DreamMachineClient(
-                self.address, self.port, self.username, self.password, self.verify_ssl
+        else:
+            self.port = 443
+            self.session = UniFiOSClient(
+                self.protocol,
+                self.address,
+                self.port,
+                self.username,
+                self.password,
+                self.verify_ssl,
             )
 
-    # TODO
     def get_camera_list(self):
-        get_camera_list(self.session)
+        return get_camera_list(self.session)
+
+    def get_session(self):
+        return self.session
+
+    def get_motion_event_list(self, start, end):
+        return get_motion_event_list(self.session, start, end)
 
 
-class ProtectError(object):
-    pass
+# TODO
+# class ProtectError(object):
+#     pass
--- protect_archiver/client/legacy.py
+++ protect_archiver/client/legacy.py
@@ -1,15 +1,21 @@
 import logging
+
 import requests
 
-from protect_archiver.config import Config
 from protect_archiver.errors import Errors
-from protect_archiver.downloader import Downloader
 
 
-class CloudKeyClient:
+class LegacyClient:
     def __init__(
-        self, address: str, port: int, username: str, password: str, verify_ssl: bool,
+        self,
+        protocol: str,
+        address: str,
+        port: int,
+        username: str,
+        password: str,
+        verify_ssl: bool,
     ):
+        self.protocol = protocol
         self.address = address
         self.port = port
         self.username = username
@@ -19,18 +25,22 @@ class CloudKeyClient:
         self._access_key = None
         self._api_token = None
 
+        self.authority = f"{self.protocol}://{self.address}:{self.port}"
+        self.base_path = "/api"
+
     # get bearer token using username and password of local user
     def fetch_api_token(self) -> str:
-        auth_uri = f"https://{self.address}:{self.port}/api/auth"
+        auth_uri = f"{self.protocol}://{self.address}:{self.port}/api/auth"
+
         response = requests.post(
             auth_uri,
             json={"username": self.username, "password": self.password},
             verify=self.verify_ssl,
         )
-        print(response.status_code)
+
         if response.status_code != 200:
             if response.status_code == 404:
-                logging.info("404 -- UDM?")
+                logging.info("404 -- UniFi OS?")
                 # GET https://192.168.2.1/api/users/self, 401 if not logged in, 200 and user data if logged in
                 # GET https://192.168.2.1/api/system, 200 {"hardware": {"shortname": "UDMPRO"}, "name": "UDM Pro"}
                 # clear session cookies, then POST to https://192.168.2.1/api/auth/login with JSON payload user/pass
@@ -39,10 +49,13 @@ class CloudKeyClient:
                     f"Authentication as user {self.username} failed "
                     f"with status {response.status_code} {response.reason}"
                 )
-            Downloader.print_download_stats()  # TODO
+            # Downloader.print_download_stats()  # TODO
             raise Errors.ProtectError(2)
+
         logging.info(f"Successfully authenticated as user {self.username}")
+
         authorization_header = response.headers["Authorization"]
+
         assert authorization_header
         return authorization_header
 
